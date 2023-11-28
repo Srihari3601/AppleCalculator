0431865f6018f42dd9ed8f911e6bfe187ff80d8b
Srihari3601 <152282624+Srihari3601@users.noreply.github.com>
2023-11-28 17:57:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let string="";
+let buttons=document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML=="="){
+            try{
+            string=eval(string);
+            document.querySelector('.input').value=string;}
+            catch(error){
+                document.querySelector('.input').value="ERROR";
+            }
+
+        }
+        else if(e.target.innerHTML=="C"){
+            string="";
+            document.querySelector('.input').value=string;
+
+        }
+        else{
+        console.log(e.target.innerHTML);
+        string=string+e.target.innerHTML;
+        document.querySelector('.input').value=string;
+        }
+
+    })
+})
